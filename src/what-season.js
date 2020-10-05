c2908a14bef8ba58c5b4c6cc00538f9800ded1fb
Alexandr Pavliychenkov <a.pavliychenkov@gmail.com>
2020-10-06 00:49:15 +0300

    feat: what season half part

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === null) {
+        return 'Unable to determine the time of year!'
+    }
+    if (typeof date.getMonth !== 'function'){
+      throw new Error("THROWN")
+    }
+
+
+    const season = {
+        '0': 'winter',
+        '1': 'winter',
+        '2': 'spring',
+        '3': 'spring',
+        '4': 'spring',
+        '5': 'summer',
+        '6': 'summer',
+        '7': 'summer',
+        '8': 'autumn (fall)',
+        '9': 'autumn (fall)',
+        '10': 'autumn (fall)',
+        '11': 'winter',
+    };
+
+    const month = date.getMonth();
+
+    let seasonElement = season[month];
+
+    return seasonElement
 };
