41037db9052a00ebfa9dc1651c6db221bec07dc7
Alexandr Pavliychenkov <a.pavliychenkov@gmail.com>
2020-10-06 00:24:46 +0300

    feat: added count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+
+    let result = 0;
+
+    [...matrix].forEach(value => {
+      [...value].forEach(value1 => {
+        if (value1 === "^^") result++
+      })
+    });
+
+    return result;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false
+    }
+    let result = '';
+
+    [...members].forEach(value => {
+        if (typeof value === 'string' && value !== '') {
+            let valueElement = value.trim()[0];
+            if (valueElement !== '' || valueElement !== ' ') {
+                result += valueElement.toUpperCase()
+            }
+        }
+    });
+    if (result === '') return;
+
+    return result.split('').sort().reduce((previousValue, currentValue) => previousValue + currentValue);
 };
